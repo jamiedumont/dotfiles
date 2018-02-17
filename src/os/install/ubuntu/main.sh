fe65998614ad22472eda95b0552e33c3da7ac9f0
Jamie Dumont <jamiedumont@icloud.com>
2018-02-17 18:38:38 +0000

    Remove plocal copies of Vim plugins

--- src/os/install/ubuntu/main.sh
+++ src/os/install/ubuntu/main.sh
@@ -13,10 +13,6 @@ upgrade
 ./git.sh
 ./tmux.sh
 
-# Vim plug
-curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
-    https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
-
 # FZF
 git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf
 ~/.fzf/install
