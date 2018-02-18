7ffa76677080ce76e9f31634f1f49617d7b9cacc
Jamie Dumont <jamiedumont@icloud.com>
2018-02-18 12:29:18 +0000

    Clean up docker install

--- src/os/install/ubuntu/docker.sh
+++ src/os/install/ubuntu/docker.sh
@@ -19,11 +19,11 @@ curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
 
 sudo apt-key fingerprint 0EBFCD88
 
-sudo add-apt-repository \
+add_apt_repo \
    "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
    $(lsb_release -cs) \
    stable"
 
-sudo apt-get update
+update
 
-sudo apt-get install docker-ce
+install_package "docker-ce" "docker-ce"
