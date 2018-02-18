7216f8215204998292e58f2188ddcefc820bbf4f
Jamie Dumont <jamiedumont@icloud.com>
2018-02-18 13:14:49 +0000

    Add docker-compose

--- src/os/install/ubuntu/docker.sh
+++ src/os/install/ubuntu/docker.sh
@@ -20,3 +20,7 @@ sudo add-apt-repository \
 update
 
 install_package "Docker" "docker-ce"
+
+execute "sudo curl -L https://github.com/docker/compose/releases/download/1.19.0/docker-compose-`uname -s`-`uname -m` -o /usr/local/bin/docker-compose" "Install docker compose"
+
+execute "sudo chmod +x /usr/local/bin/docker-compose" "Make executable"
